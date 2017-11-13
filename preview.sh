a6a58282706379c6c66400fa4fee19aa7be6a5a0
Masataro Asai <guicho2.71828@gmail.com>
2017-11-13 09:37:44 +0900

    update preview

--- preview.sh
+++ preview.sh
@@ -5,5 +5,6 @@
 cd deploy
 while true
 do
-    clackup <(echo "(lack:builder (:static :path #'identity) #'identity)")
+    clackup <(echo "(lack:builder :accesslog (:static :path (lambda (p) (print p) (if (string= p \"/\") \"/index.html\" p))) #'identity)")
+    # clackup <(echo "(lack:builder (:static :path #'identity) #'identity)")
 done
