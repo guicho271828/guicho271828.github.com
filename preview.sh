38084f2c904492aa6d77711ad9e4a83490376d81
Masataro Asai <guicho2.71828@gmail.com>
2017-11-13 12:22:15 +0900

    preview.sh

--- preview.sh
+++ preview.sh
@@ -5,6 +5,7 @@
 cd deploy
 while true
 do
-    clackup <(echo "(lack:builder :accesslog (:static :path (lambda (p) (print p) (if (string= p \"/\") \"/index.html\" p))) #'identity)")
+    clackup <(echo "(lack:builder :accesslog (:static :path (lambda (p) (print p) (if (char= #\/ (alexandria:last-elt p)) (concatenate 'string p \"index.html\") p))) #'identity)")
+    # clackup <(echo "(lack:builder :accesslog (:static :path (lambda (p) (print p) (if (string= p \"/\") \"/index.html\" p))) #'identity)")
     # clackup <(echo "(lack:builder (:static :path #'identity) #'identity)")
 done
